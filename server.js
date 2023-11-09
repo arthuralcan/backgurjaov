923e365a681a3290dd3158505e02c4874837eb8c
Arthur Alcantara de Andrade <142822030+arthuralcan@users.noreply.github.com>
2023-11-09 12:28:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-const express =  require('express'); 
+const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
 
 const routes = require('./src/routes');
-const { ppid } = require('process');
 
-const app = express()
+const app = express();
 
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://backendgurjao01:1473570a@mongodb.backendgurjao.kinghost.net/backendgurjao01', 
-async(err) => {
-    if(err) throw err;
-    console.log('connected to MongoDB')
-});
 
-// views 
+mongoose.connect('mongodb://backendgurjao01:1473570a@mongodb.backendgurjao.kinghost.net/backendgurjao01')
+  .then(() => {
+    console.log('Conectado ao MongoDB');
+    
+    // Restante do seu código que depende da conexão com o MongoDB
+    app.use(cors());
+    app.use(cookieParser());
+    app.use(express.json()); // Formato que trocaremos as informações do back para o front (JSON)
+    app.use(routes); // Caminho para as rotas
 
-app.use(cors());
-app.use(cookieParser());
-app.use(express.json()); //formato que trocaremos as info back > front vai ser json
-app.use(routes); // caminho pra rotas
-
-app.listen(3003, function(){ // startar o server 
-    console.log("Servidor iniciado com sucesso!")
-})
+    app.listen(3003, function () {
+      console.log('Servidor iniciado com sucesso!');
+    });
+  })
+  .catch((err) => {
+    console.error('Erro ao conectar ao MongoDB:', err.message);
+  });
